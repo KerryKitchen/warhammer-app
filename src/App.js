8dad1ec998f71a815ac4d3d59f81cc0da927c544
Kerry <kerry.kitchen@hotmail.com>
2023-01-15 18:24:08 +1000

    Added Necron Sounds

--- src/App.js
+++ src/App.js
@@ -20,6 +20,28 @@ import plasma_pistol_audio from './assets/audio/space-marines/plasma-pistol.mp3'
 import plasma_gun_audio from './assets/audio/space-marines/plasma-gun.mp3';
 import biker_hit_audio from './assets/audio/space-marines/biker-hit.mp3';
 
+// Necron sound files
+import gauss_flayer_audio from './assets/audio/necrons/gauss-flayer.mp3';
+import gauss_reaper_audio from './assets/audio/necrons/gauss-reaper.mp3';
+import glaive_audio from './assets/audio/necrons/glaive.mp3';
+import necron_die_audio from './assets/audio/necrons/necron-die.mp3';
+import necron_die_2_audio from './assets/audio/necrons/necron-die-2.mp3';
+import necron_die_3_audio from './assets/audio/necrons/necron-die-3.mp3';
+import necron_die_4_audio from './assets/audio/necrons/necron-die-4.mp3';
+import necron_hit_audio from './assets/audio/necrons/necron-hit.mp3';
+import necron_hit_2_audio from './assets/audio/necrons/necron-hit-2.mp3';
+import necron_hit_3_audio from './assets/audio/necrons/necron-hit-3.mp3';
+import necron_hit_4_audio from './assets/audio/necrons/necron-hit-4.mp3';
+import necron_melee_audio from './assets/audio/necrons/necron-melee.mp3';
+import necron_melee_2_audio from './assets/audio/necrons/necron-melee-2.mp3';
+import necron_move_audio from './assets/audio/necrons/necron-move.mp3';
+import reap_blade_audio from './assets/audio/necrons/reap-blade.mp3';
+import scarab_attack_audio from './assets/audio/necrons/scarab-attack.mp3';
+import scarab_die_audio from './assets/audio/necrons/scarab-die.mp3';
+import scarab_move_audio from './assets/audio/necrons/scarab-move.mp3';
+import tachyon_arrow_audio from './assets/audio/necrons/tachyon-arrow.mp3';
+import threshers_audio from './assets/audio/necrons/threshers.mp3';
+
 // UI icons
 import ap from './assets/images/ap.png';
 import blast from './assets/images/blast.png';
@@ -32,24 +54,48 @@ import ws from './assets/images/ws.png';
 //
 function App() {
 
+  //
   const user_volume = 0.5;
 
-  const [bolter_pistol] = useSound(bolter_pistol_audio, { volume: user_volume })
+  // Space Marine sounds
+  const [bolter_pistol] = useSound(bolter_pistol_audio, { volume: user_volume });
   const [bolter_heavy] = useSound(bolter_heavy_audio, { volume: user_volume });
-  const [chainsword] = useSound(chainsword_audio, { volume: user_volume })
-  const [grenade] = useSound(grenade_audio, { volume: user_volume })
-  const [biker_move] = useSound(biker_move_audio, { volume: user_volume })
-  const [deflect_hit] = useSound(deflect_hit_audio, { volume: user_volume })
-  const [infantry_take_damage] = useSound(infantry_take_damage_audio, { volume: user_volume })
-  const [infantry_take_damage_2] = useSound(infantry_take_damage_2_audio, { volume: user_volume })
-  const [biker_die] = useSound(biker_die_audio, { volume: user_volume })
-  const [power_sword] =  useSound(power_sword_audio, { volume: user_volume })
-  const [infantry_move] = useSound(infantry_move_audio, { volume:user_volume })
-  const [infantry_die] = useSound(infantry_die_audio, { volume:user_volume })
-  const [infantry_die_2] = useSound(infantry_die_2_audio, { volume:user_volume })
-  const [plasma_pistol] = useSound(plasma_pistol_audio, { volume:user_volume })
-  const [plasma_gun] = useSound(plasma_gun_audio, { volume:user_volume })
-  const [biker_hit] = useSound(biker_hit_audio, { volume:user_volume })
+  const [chainsword] = useSound(chainsword_audio, { volume: user_volume });
+  const [grenade] = useSound(grenade_audio, { volume: user_volume });
+  const [biker_move] = useSound(biker_move_audio, { volume: user_volume });
+  const [deflect_hit] = useSound(deflect_hit_audio, { volume: user_volume });
+  const [infantry_take_damage] = useSound(infantry_take_damage_audio, { volume: user_volume });
+  const [infantry_take_damage_2] = useSound(infantry_take_damage_2_audio, { volume: user_volume });
+  const [biker_die] = useSound(biker_die_audio, { volume: user_volume });
+  const [power_sword] =  useSound(power_sword_audio, { volume: user_volume });
+  const [infantry_move] = useSound(infantry_move_audio, { volume:user_volume });
+  const [infantry_die] = useSound(infantry_die_audio, { volume:user_volume });
+  const [infantry_die_2] = useSound(infantry_die_2_audio, { volume:user_volume });
+  const [plasma_pistol] = useSound(plasma_pistol_audio, { volume:user_volume });
+  const [plasma_gun] = useSound(plasma_gun_audio, { volume:user_volume });
+  const [biker_hit] = useSound(biker_hit_audio, { volume:user_volume });
+
+  // Necron sounds
+  const [gauss_flayer] = useSound(gauss_flayer_audio, { volume:user_volume });
+  const [gauss_reaper] = useSound(gauss_reaper_audio, { volume:user_volume });
+  const [glaive] = useSound(glaive_audio, { volume:user_volume });
+  const [necron_die] = useSound(necron_die_audio, { volume:user_volume });
+  const [necron_die_2] = useSound(necron_die_2_audio, { volume:user_volume });
+  const [necron_die_3] = useSound(necron_die_3_audio, { volume:user_volume });
+  const [necron_die_4] = useSound(necron_die_4_audio, { volume:user_volume });
+  const [necron_hit] = useSound(necron_hit_audio, { volume:user_volume });
+  const [necron_hit_2] = useSound(necron_hit_2_audio, { volume:user_volume });
+  const [necron_hit_3] = useSound(necron_hit_3_audio, { volume:user_volume });
+  const [necron_hit_4] = useSound(necron_hit_4_audio, { volume:user_volume });
+  const [necron_melee] = useSound(necron_melee_audio, { volume:user_volume });
+  const [necron_melee_2] = useSound(necron_melee_2_audio, { volume:user_volume });
+  const [necron_move] = useSound(necron_move_audio, { volume:user_volume });
+  const [reap_blade] = useSound(reap_blade_audio, { volume:user_volume });
+  const [scarab_attack] = useSound(scarab_attack_audio, { volume:user_volume });
+  const [scarab_move] = useSound(scarab_move_audio, { volume:user_volume });
+  const [scarab_die] = useSound(scarab_die_audio, { volume:user_volume });
+  const [tachyon_arrow] = useSound(tachyon_arrow_audio, { volume:user_volume });
+  const [threshers] = useSound(threshers_audio, { volume:user_volume });
 
   return (
 
@@ -157,15 +203,15 @@ function App() {
               <span className="desc desc-last">Hyperphase glaive</span>
             </div>
             <div className="unit-equipment-btns space-y-2">
-              <img className="icon-button" src={bs} alt="icon"/>
-              <img className="icon-button" src={ws} alt="icon"/>
+              <img className="icon-button" src={bs} alt="icon" onClick={() => tachyon_arrow()}/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => glaive()}/>
             </div>
           </div>
           <div className="unit-actions justify-between">
-            <img className="icon-button" src={mv} alt="icon"/>
+            <img className="icon-button" src={mv} alt="icon" onClick={() => necron_move()}/>
             <img className="icon-button" src={sv} alt="icon" onClick={() => deflect_hit()}/>
-            <img className="icon-button" src={ap} alt="icon"/>
-            <img className="icon-button" src={dead} alt="icon"/>
+            <img className="icon-button" src={ap} alt="icon" onClick={() => necron_hit()}/>
+            <img className="icon-button" src={dead} alt="icon" onClick={() => necron_die()}/>
           </div>
         </div> {/* end unit-card */}
 
@@ -180,16 +226,16 @@ function App() {
               <span className="desc desc-last">Melee</span>
             </div>
             <div className="unit-equipment-btns space-y-2">
-              <img className="icon-button" src={bs} alt="icon"/>
-              <img className="icon-button" src={bs} alt="icon"/>
-              <img className="icon-button" src={ws} alt="icon"/>
+              <img className="icon-button" src={bs} alt="icon" onClick={() => gauss_flayer()}/>
+              <img className="icon-button" src={bs} alt="icon" onClick={() => gauss_reaper()}/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => necron_melee()}/>
             </div>
           </div>
           <div className="unit-actions justify-between">
-            <img className="icon-button" src={mv} alt="icon"/>
+            <img className="icon-button" src={mv} alt="icon" onClick={() => necron_move()}/>
             <img className="icon-button" src={sv} alt="icon" onClick={() => deflect_hit()}/>
-            <img className="icon-button" src={ap} alt="icon"/>
-            <img className="icon-button" src={dead} alt="icon"/>
+            <img className="icon-button" src={ap} alt="icon" onClick={() => necron_hit_2()}/>
+            <img className="icon-button" src={dead} alt="icon" onClick={() => necron_die_2()}/>
           </div>
         </div> {/* end unit-card */}
 
@@ -203,15 +249,15 @@ function App() {
               <span className="desc desc-last">Hyperphase threshers</span>
             </div>
             <div className="unit-equipment-btns space-y-2">
-              <img className="icon-button" src={ws} alt="icon"/>
-              <img className="icon-button" src={ws} alt="icon"/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => reap_blade()}/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => threshers()}/>
             </div>
           </div>
           <div className="unit-actions justify-between">
-            <img className="icon-button" src={mv} alt="icon"/>
+            <img className="icon-button" src={mv} alt="icon" onClick={() => necron_move()}/>
             <img className="icon-button" src={sv} alt="icon" onClick={() => deflect_hit()}/>
-            <img className="icon-button" src={ap} alt="icon"/>
-            <img className="icon-button" src={dead} alt="icon"/>
+            <img className="icon-button" src={ap} alt="icon" onClick={() => necron_hit_3()}/>
+            <img className="icon-button" src={dead} alt="icon" onClick={() => necron_die_3()}/>
           </div>
         </div> {/* end unit-card */}
 
@@ -224,14 +270,14 @@ function App() {
               <span className="desc desc-last">Monomolecular proboscis</span>
             </div>
             <div className="unit-equipment-btns space-y-2">
-              <img className="icon-button" src={ws} alt="icon"/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => necron_melee_2()}/>
             </div>
           </div>
           <div className="unit-actions justify-between">
-            <img className="icon-button" src={mv} alt="icon"/>
+            <img className="icon-button" src={mv} alt="icon" onClick={() => necron_move()}/>
             <img className="icon-button" src={sv} alt="icon" onClick={() => deflect_hit()}/>
-            <img className="icon-button" src={ap} alt="icon"/>
-            <img className="icon-button" src={dead} alt="icon"/>
+            <img className="icon-button" src={ap} alt="icon" onClick={() => necron_hit_4()}/>
+            <img className="icon-button" src={dead} alt="icon" onClick={() => necron_die_4()}/>
           </div>
         </div> {/* end unit-card */}
 
@@ -244,14 +290,14 @@ function App() {
               <span className="desc desc-last">Feeder mandibles</span>
             </div>
             <div className="unit-equipment-btns space-y-2">
-              <img className="icon-button" src={ws} alt="icon"/>
+              <img className="icon-button" src={ws} alt="icon" onClick={() => scarab_attack()}/>
             </div>
           </div>
           <div className="unit-actions justify-between">
-            <img className="icon-button" src={mv} alt="icon"/>
+            <img className="icon-button" src={mv} alt="icon" onClick={() => scarab_move()}/>
             <img className="icon-button" src={sv} alt="icon" onClick={() => deflect_hit()}/>
-            <img className="icon-button" src={ap} alt="icon"/>
-            <img className="icon-button" src={dead} alt="icon"/>
+            <img className="icon-button" src={ap} alt="icon" onClick={() => necron_hit_2()}/>
+            <img className="icon-button" src={dead} alt="icon" onClick={() => scarab_die()}/>
           </div>
         </div> {/* end unit-card */}
 
